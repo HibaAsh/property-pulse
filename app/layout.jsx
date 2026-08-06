c40f292b41f094527174217238897b04028cac04
hiba <hibashakoof@gmail.com>
2026-08-06 09:58:11 +0300

    Created layout, homePage and styles

---
+++ app/layout.jsx
@@ -0,0 +1,23 @@
+import React from 'react'
+
+import '@/assets/styles/globals.css'
+
+export const metadata = {
+  title: 'Property Pulse | Find The Perfect Rental',
+  description: 'Fidn your dream rental property',
+  keywords: 'rental, find rentals, find properties'
+}
+
+const MainLayout = ({ children }) => {
+  return (
+    <html lang='en'>
+      <body>
+        <main>
+          {children}
+        </main>
+      </body>
+    </html>
+  )
+}
+
+export default MainLayout
